42faf5f7304775cdb1390da1067c7511d23c9158
Peekmo <axel.anceau@gmail.com>
2014-02-04 23:33:30 +0100

    Parsing variables (need to be improved)

--- src/wx/templating/Template.hx
+++ src/wx/templating/Template.hx
@@ -19,4 +19,27 @@ class Template
     {
         this.resource = resource;
     }
+
+    public function execute(?parameters: Dynamic) : String
+    {
+        return this.print(parameters);
+    }
+
+    private function print(parameters: Dynamic) : String
+    {
+        var data: String = this.resource;
+        var reg = ~/{{[ ]*([a-z]+)[ ]*}}/g;
+
+        while (null != data) {
+            if (!reg.match(data)) break;
+
+            data = reg.matchedRight();
+            var vReg = new EReg('{{[ ]*(' + StringTools.trim(reg.matched(1)) + ')[ ]*}}', '');
+
+            this.resource = vReg.replace(this.resource, Reflect.field(parameters, reg.matched(1)));
+        }
+
+        return this.resource;
+        //return reg.replace(this.resource, Std.string(Reflect.field(parameters, "$1")));
+    }
 }
