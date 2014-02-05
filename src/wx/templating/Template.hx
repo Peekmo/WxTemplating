228938a67133bd96197993fe151c7787de4b731d
Peekmo <axel.anceau@gmail.com>
2014-02-05 21:27:54 +0100

    Multiple variable level parsing

--- src/wx/templating/Template.hx
+++ src/wx/templating/Template.hx
@@ -12,6 +12,11 @@ class Template
     public var resource(null, null): String;
 
     /**
+     * @var parameters: Parameters passed to the template
+     */
+    public var parameters(null, null): Dynamic;
+
+    /**
      * Constructor - Sets resource to execute
      * @param  resource: String        Template
      */
@@ -20,12 +25,22 @@ class Template
         this.resource = resource;
     }
 
+    /**
+     * Method called to start template's parsing
+     * @param  ?parameters: Dynamic       Parameters sent to the template
+     * @return              Template parsed
+     */
     public function execute(?parameters: Dynamic) : String
     {
-        return this.print(parameters);
+        this.parameters = parameters;
+        return this.print();
     }
 
-    private function print(parameters: Dynamic) : String
+    /**
+     * Parse all print symbols on the template ( {{ var }} )
+     * @return Resource modified
+     */
+    private function print() : String
     {
         var data: String = this.resource;
         var reg = ~/{{[ ]*([^{]+)[ ]*}}/g;
@@ -36,10 +51,35 @@ class Template
             data = reg.matchedRight();
             var vReg = new EReg('{{[ ]*(' + StringTools.trim(reg.matched(1)) + ')[ ]*}}', '');
 
-            this.resource = vReg.replace(this.resource, Reflect.field(parameters, reg.matched(1)));
+            this.resource = vReg.replace(this.resource, this.parseVariable(reg.matched(1)));
         }
 
         return this.resource;
-        //return reg.replace(this.resource, Std.string(Reflect.field(parameters, "$1")));
+    }
+
+    /**
+     * Parse a variable
+     * @param  variable: String        Full name of the variable (e.g : user.name)
+     * @param  ?object:  Dynamic       Source object of attributes (e.g : user for name attribute)
+     * @return           Result or an other object
+     */
+    private function parseVariable(variable: String, ?object: Dynamic) : Dynamic
+    {
+        if (null == object) {
+            object = this.parameters;
+        }
+
+        var pieces : Array<String> = StringTools.trim(variable).split('.');
+        var data = Reflect.field(object, pieces[0]);
+
+        if (1 == pieces.length) {
+            return data;
+        }
+
+        // Remove the current var
+        pieces.shift();
+
+        // Work on the next variable
+        return this.parseVariable(pieces.join('.'), data);
     }
 }
